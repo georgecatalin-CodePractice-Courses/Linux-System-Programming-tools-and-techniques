2c9acb6af02540ebde5383dbdad5c4336f0966dd
George Calin <george.calin@gmail.com>
2021-09-28 14:33:19 +0300

    An example of recursive dependency

---
+++ Correct_way_of_using_structures_and_functions/Recursive_Dependency/an_example_of_recursive_dependency.c
@@ -0,0 +1,13 @@
+struct employee_t
+{
+	char name[50];
+	unsigned int employee_id;
+	occupation_t occupation;    //here is an example of recursive dependency as the compiler needs to know the size of the struct occupation_t (which is the sum of all its components). But the compiler does not know the size of occupation_t because it has not been declared yet
+};
+
+struct ocuppation_t 
+{
+	char designation[30];
+	unsigned int salary;
+	employee_t manager;  //here is also an example of recursive dependency to another struct
+}
